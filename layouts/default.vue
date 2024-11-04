3fd0bc1b05c7d43b50d4a502df88800e82d95ea3
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2024-11-04 10:50:56 +0700

    layout, percantik product, and modify filter function

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="flex h-screen">
-    <div class="flex flex-col flex-1 overflow-hidden">
-      <MainHeader @open-navbar="navbar = true" />
-      <div class="container mx-auto px-4 py-5">
-        <slot />
-      </div>
-      <USlideover v-model="navbar" side="left" :ui="{ background: 'bg-[#3F3F3F]' }">
-        <div class="p-4 flex-1 text-white">
-          <UButton color="gray" variant="ghost" size="sm" icon="i-heroicons-x-mark-20-solid"
-            class="flex sm:hidden absolute end-5 top-5 z-10" square padded @click="navbar = false" />
-          <UVerticalNavigation :links="links" :ui="{
-            width: 'w-fit',
-            active: '',
-            inactive: ''
-          }">
-            <template #default="{ link }">
-              <span class="group text-white font-bold text-xl relative flex flex-wrap items-center gap-x-2">
-                <span class="flex-grow flex flex-col">
-                  <span>{{ link.label }}</span>
-                  <span class="flex-grow max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
-                </span>
-                <UIcon name="i-heroicons-chevron-right"
-                  class="transition-transform group-hover:translate-x-7 duration-500 ease-out flex-grow" />
-              </span>
-            </template>
-          </UVerticalNavigation>
-        </div>
-      </USlideover>
+  <div class="flex flex-col h-screen overflow-hidden">
+    <MainHeader @open-navbar="navbar = true" />
+    <div class="container h-full mx-auto px-4 py-5">
+      <slot />
     </div>
+    <USlideover v-model="navbar" side="left" :ui="{ background: 'bg-[#3F3F3F]' }">
+      <div class="p-4 flex-1 text-white">
+        <UButton color="gray" variant="ghost" size="sm" icon="i-heroicons-x-mark-20-solid"
+          class="flex sm:hidden absolute end-5 top-5 z-10" square padded @click="navbar = false" />
+        <UVerticalNavigation :links="links" :ui="{
+          width: 'w-fit',
+          active: '',
+          inactive: ''
+        }">
+          <template #default="{ link }">
+            <span class="group text-white font-bold text-xl relative flex flex-wrap items-center gap-x-2">
+              <span class="flex-grow flex flex-col">
+                <span>{{ link.label }}</span>
+                <span
+                  class="flex-grow max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+              </span>
+              <UIcon name="i-heroicons-chevron-right"
+                class="transition-transform group-hover:translate-x-7 duration-500 ease-out flex-grow" />
+            </span>
+          </template>
+        </UVerticalNavigation>
+      </div>
+    </USlideover>
   </div>
 </template>
 
--- pages/produk.vue
+++ pages/produk.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="flex flex-col flex-grow gap-y-5">
+  <div class="flex flex-col flex-grow gap-y-10">
     <div class="w-full flex justify-between gap-3">
-      <div class="flex-1">
+      <div class="flex-[.5]">
         <UInput v-model="searchQuery" placeholder="Cari nama produk..." icon="heroicons:magnifying-glass-20-solid" />
       </div>
-      <div class="flex-1 flex justify-center gap-x-5">
-        <UButton :variant="activeButtonJurusan('PPLG')" color="green" class="rounded-full" @click="selectJurusan('PPLG')" label="PPLG" />
-        <UButton :variant="activeButtonJurusan('TJKT')" color="blue" class="rounded-full" @click="selectJurusan('TJKT')" label="TJKT" />
-        <UButton :variant="activeButtonJurusan('TSM')" color="red" class="rounded-full" @click="selectJurusan('TSM')" label="TSM" />
-        <UButton :variant="activeButtonJurusan('DKV')" color="orange" class="rounded-full" @click="selectJurusan('DKV')" label="DKV" />
-        <UButton :variant="activeButtonJurusan('TOI')" color="gray" class="rounded-full" @click="selectJurusan('TOI')" label="TOI" />
+      <div class="flex justify-center gap-x-5">
+        <UButton :variant="activeButtonJurusan('PPLG')" color="green" class="rounded-full"
+          @click="selectJurusan('PPLG')" label="PPLG" />
+        <UButton :variant="activeButtonJurusan('TJKT')" color="blue" class="rounded-full" @click="selectJurusan('TJKT')"
+          label="TJKT" />
+        <UButton :variant="activeButtonJurusan('TSM')" color="red" class="rounded-full" @click="selectJurusan('TSM')"
+          label="TSM" />
+        <UButton :variant="activeButtonJurusan('DKV')" color="orange" class="rounded-full" @click="selectJurusan('DKV')"
+          label="DKV" />
+        <UButton :variant="activeButtonJurusan('TOI')" color="gray" class="rounded-full" @click="selectJurusan('TOI')"
+          label="TOI" />
       </div>
     </div>
     <div v-if="status == 'pending' || status == 'error'">
@@ -21,18 +26,24 @@
         {{ error.message }}
       </div>
     </div>
-    <div v-else class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-y-5">
-      <div v-for="product in filteredProducts" :key="product.id" class="flex">
-        <UCard class="flex flex-col" :class="borderClass(product.kelompok.kelas.nama)" :ui="{ header: { base: 'flex-grow flex items-center' } }">
-          <template #header>
+    <div v-else class="min-h-full grid grid-cols-2 md:grid-cols-3 gap-x-10 gap-y-8">
+      <div v-for="product in filteredProducts" :key="product.id">
+        <UCard class="h-full shadow-lg" :class="borderClass(product.kelompok.kelas.nama)"
+          :ui="{ body: { base: 'h-full grid grid-flow-col grid-cols-2 grid-rows-2 gap-5' } }">
+          <div class="flex flex-col">
+            <div class="font-bold text-lg">{{ product.nama }}</div>
+            <div class="text-lg text-red-500">{{ rupiah(product.harga) }}</div>
+            <div class="text-sm">Stok: {{ product.sisa }}</div>
+          </div>
+          <div class="flex items-end me-5">
+            <UButton class="w-full justify-center" label="Add to cart" />
+          </div>
+          <div class="row-span-2 flex justify-center items-center">
             <NuxtImg v-if="product.fotoUrl" :src="product.fotoUrl" width="300" />
             <NuxtImg v-else src="img/img-placeholder.png" width="300" />
-          </template>
-          <div class="text-center font-bold text-lg">{{ product.nama }}</div>
-          <div v-if="product.kelompok" class="text-center text-gray-500 text-sm">{{ product.kelompok.nama }} / {{
-            product.kelompok.kelas.nama }}</div>
-          <div class="text-sm">Stok: {{ product.sisa }}</div>
-          <div class="text-sm">Harga: {{ rupiah(product.harga) }}</div>
+          </div>
+          <!-- <div v-if="product.kelompok" class="text-center text-gray-500 text-sm">{{ product.kelompok.nama }} / {{
+            product.kelompok.kelas.nama }}</div> -->
         </UCard>
       </div>
     </div>
@@ -44,11 +55,11 @@ const supabase = useSupabaseClient()
 
 const borderClass = (kelas) => {
   return {
-    'border border-green-500': (kelas.includes('PPLG')),
-    'border border-blue-500': (kelas.includes('TJKT')),
-    'border border-red-500': (kelas.includes('TSM')),
-    'border border-orange-500': (kelas.includes('DKV')),
-    'border border-gray-500': (kelas.includes('TOI'))
+    'border border-green-500 shadow-green-500': (kelas.includes('PPLG')),
+    'border border-blue-500 shadow-blue-500': (kelas.includes('TJKT')),
+    'border border-red-500 shadow-red-500': (kelas.includes('TSM')),
+    'border border-orange-500 shadow-orange-500': (kelas.includes('DKV')),
+    'border border-gray-500 shadow-gray-500': (kelas.includes('TOI'))
   }
 }
 
@@ -84,14 +95,14 @@ const availableProducts = computed(() => products.value.filter(product => produc
 
 const searchQuery = ref('')
 const selectedJurusan = ref('')
-const selectJurusan = jurusan => selectedJurusan.value = jurusan
+const selectJurusan = jurusan => selectedJurusan.value === jurusan ? selectedJurusan.value = '' : selectedJurusan.value = jurusan
 const activeButtonJurusan = jurusan => selectedJurusan.value === jurusan ? 'solid' : 'outline'
 
 const filteredProducts = computed(() => {
   const filtered = availableProducts.value.filter((product) => {
     const filterName = product.nama.toLowerCase().includes(searchQuery.value.toLowerCase())
     const filterMajor = selectedJurusan.value ? product.kelompok.kelas.nama.includes(selectedJurusan.value) : true
-    
+
     return filterName && filterMajor
   })
 
